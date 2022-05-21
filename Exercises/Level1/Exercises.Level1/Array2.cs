c606579ad805961eea487241638a54ad1c217959
Sandra Eglīte <san.eglite@gmail.com>
2022-05-21 17:23:58 +0300

    21May edit

--- Exercises/Level1/Exercises.Level1/Array2.cs
+++ Exercises/Level1/Exercises.Level1/Array2.cs
@@ -360,16 +360,15 @@ public class Array2
                 }
             }
 
-            if (two = four)
-            {
-                return false;
-            }
-            else if ((!two && four) || (!four && two))
-            {
-                return true;
-            }
+
         }
-        return true;
+        if (two == four)
+        {
+            return false;
+        }
+
+        return two || four;
+
     }
 
     /// <summary>
@@ -407,271 +406,277 @@ public class Array2
         for (int i = 0; i < nums.Length - 2; i++)
         {
             if (nums[i + 1] == 7 && nums[i + 2] == 7)
-             {
-            return true;
-             }
-        
-    }
+            {
+                return true;
+            }
+
+        }
         return false;
 
     }
 
-/// <summary>
-/// Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in
-/// the array.
-/// 
-/// has12([1, 3, 2]) → true
-/// has12([3, 1, 2]) → true
-/// has12([3, 1, 4, 5, 2]) → true
-/// </summary>
-public bool Has12(int[] nums)
-{
-    throw new NotImplementedException();
-}
-
-/// <summary>
-/// Given an array of ints, return true if the array contains either 3 even or 3 odd values all
-/// next to each other.
-/// 
-/// modThree([2, 1, 3, 5]) → true
-/// modThree([2, 1, 2, 5]) → false
-/// modThree([2, 4, 2, 5]) → true
-/// </summary>
-public bool ModThree(int[] nums)
-{
-    throw new NotImplementedException();
-}
-
-/// <summary>
-/// Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and
-/// no 3's are next to each other.
-/// 
-/// haveThree([3, 1, 3, 1, 3]) → true
-/// haveThree([3, 1, 3, 3]) → false
-/// haveThree([3, 4, 3, 3, 4]) → false
-/// </summary>
-public bool HaveThree(int[] nums)
-{
-    int count = 0;
-    if (nums.Length < 3)
+    /// <summary>
+    /// Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in
+    /// the array.
+    /// 
+    /// has12([1, 3, 2]) → true
+    /// has12([3, 1, 2]) → true
+    /// has12([3, 1, 4, 5, 2]) → true
+    /// </summary>
+    public bool Has12(int[] nums)
     {
-        return false;
+        throw new NotImplementedException();
     }
-    for (int i = 0; i < nums.Length; i++)
+
+    /// <summary>
+    /// Given an array of ints, return true if the array contains either 3 even or 3 odd values all
+    /// next to each other.
+    /// 
+    /// modThree([2, 1, 3, 5]) → true
+    /// modThree([2, 1, 2, 5]) → false
+    /// modThree([2, 4, 2, 5]) → true
+    /// </summary>
+    public bool ModThree(int[] nums)
     {
-        if (nums[i] == 3 && nums[i + 1] != 3)
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and
+    /// no 3's are next to each other.
+    /// 
+    /// haveThree([3, 1, 3, 1, 3]) → true
+    /// haveThree([3, 1, 3, 3]) → false
+    /// haveThree([3, 4, 3, 3, 4]) → false
+    /// </summary>
+    public bool HaveThree(int[] nums)
+    {
+        int count = 0;
+        if (nums.Length < 3)
+        {
+            return false;
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i + 1 < nums.Length)
+            {
+                if (nums[i] == 3 && nums[i + 1] != 3)
+                    count++;
+            }
+
+        }
+        if (nums[nums.Length - 1] == 3 && nums[nums.Length - 2] != 3) 
+        {
             count++;
-        if (nums[nums.Length - 1] == 3 && nums[nums.Length - 2] != 3)
-            count++;
+        }
+
+        return (count == 3);
+
+
+
+
+
     }
-    if (count == 3)
+
+    /// <summary>
+    /// Given an array of ints, return true if every 2 that appears in the array is next to another 2.
+    /// 
+    /// twoTwo([4, 2, 2, 3]) → true
+    /// twoTwo([2, 2, 4]) → true
+    /// twoTwo([2, 2, 4, 2]) → false
+    /// </summary>
+    public bool TwoTwo(int[] nums)
     {
-        return true;
+        throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Return true if the group of N numbers at the start and end of the array are the same. For
+    /// example, with {5, 6, 45, 99, 13, 5, 6}, the ends are the same for n=0 and n=2, and false
+    /// for n=1 and n=3. You may assume that n is in the range 0..nums.length inclusive.
+    /// 
+    /// sameEnds([5, 6, 45, 99, 13, 5, 6], 1) → false
+    /// sameEnds([5, 6, 45, 99, 13, 5, 6], 2) → true
+    /// sameEnds([5, 6, 45, 99, 13, 5, 6], 3) → false
+    /// </summary>
+    public bool SameEnds(int[] nums, int val)
+    {
+        throw new NotImplementedException();
+    }
 
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return true if the array contains, somewhere, three increasing adjacent numbers like ....
+    /// 4, 5, 6, ... or 23, 24, 25.
+    /// 
+    /// tripleUp([1, 4, 5, 6, 2]) → true
+    /// tripleUp([1, 2, 3]) → true
+    /// tripleUp([1, 2, 4]) → false
+    /// </summary>
+    public bool TripleUp(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Given an array of ints, return true if every 2 that appears in the array is next to another 2.
-/// 
-/// twoTwo([4, 2, 2, 3]) → true
-/// twoTwo([2, 2, 4]) → true
-/// twoTwo([2, 2, 4, 2]) → false
-/// </summary>
-public bool TwoTwo(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Given start and end numbers, return a new array containing the sequence of integers
+    /// rom start up to but not including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}. The
+    /// end number will be greater or equal to the start number. Note that a length-0 array is
+    /// valid.
+    /// 
+    /// fizzArray3(5, 10) → [5, 6, 7, 8, 9]
+    /// fizzArray3(11, 18) → [11, 12, 13, 14, 15, 16, 17]
+    /// fizzArray3(1, 3) → [1, 2]
+    /// </summary>
+    public int[] FizzArray3(int start, int end)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return true if the group of N numbers at the start and end of the array are the same. For
-/// example, with {5, 6, 45, 99, 13, 5, 6}, the ends are the same for n=0 and n=2, and false
-/// for n=1 and n=3. You may assume that n is in the range 0..nums.length inclusive.
-/// 
-/// sameEnds([5, 6, 45, 99, 13, 5, 6], 1) → false
-/// sameEnds([5, 6, 45, 99, 13, 5, 6], 2) → true
-/// sameEnds([5, 6, 45, 99, 13, 5, 6], 3) → false
-/// </summary>
-public bool SameEnds(int[] nums, int val)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You
+    /// may modify and return the given array, or return a new array.
+    /// 
+    /// shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
+    /// shiftLeft([1, 2]) → [2, 1]
+    /// shiftLeft([1]) → [1]
+    /// </summary>
+    public int[] ShiftLeft(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return true if the array contains, somewhere, three increasing adjacent numbers like ....
-/// 4, 5, 6, ... or 23, 24, 25.
-/// 
-/// tripleUp([1, 4, 5, 6, 2]) → true
-/// tripleUp([1, 2, 3]) → true
-/// tripleUp([1, 2, 4]) → false
-/// </summary>
-public bool TripleUp(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// For each multiple of 10 in the given array, change all the values following it to be that
+    /// multiple of 10, until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2,
+    /// 10, 10, 10, 20, 20}.
+    /// 
+    /// tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+    /// tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
+    /// tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
+    /// </summary>
+    public int[] TenRun(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Given start and end numbers, return a new array containing the sequence of integers
-/// rom start up to but not including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}. The
-/// end number will be greater or equal to the start number. Note that a length-0 array is
-/// valid.
-/// 
-/// fizzArray3(5, 10) → [5, 6, 7, 8, 9]
-/// fizzArray3(11, 18) → [11, 12, 13, 14, 15, 16, 17]
-/// fizzArray3(1, 3) → [1, 2]
-/// </summary>
-public int[] FizzArray3(int start, int end)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Given a non-empty array of ints, return a new array containing the elements from the
+    /// original array that come before the first 4 in the original array. The original array will
+    /// contain at least one 4. Note that it is possible to create an array of length 0.
+    /// 
+    /// pre4([1, 2, 4, 1]) → [1, 2]
+    /// pre4([3, 1, 4]) → [3, 1]
+    /// pre4([1, 4, 4]) → [1]
+    /// </summary>
+    public int[] Pre4(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You
-/// may modify and return the given array, or return a new array.
-/// 
-/// shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
-/// shiftLeft([1, 2]) → [2, 1]
-/// shiftLeft([1]) → [1]
-/// </summary>
-public int[] ShiftLeft(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Given a non-empty array of ints, return a new array containing the elements from the
+    /// original array that come after the last 4 in the original array. The original array will contain
+    /// at least one 4. Note that it is possible to create an array of length 0.
+    /// 
+    /// post4([2, 4, 1, 2]) → [1, 2]
+    /// post4([4, 1, 4, 2]) → [2]
+    /// post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+    /// </summary>
+    public int[] Post4(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// For each multiple of 10 in the given array, change all the values following it to be that
-/// multiple of 10, until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2,
-/// 10, 10, 10, 20, 20}.
-/// 
-/// tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
-/// tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
-/// tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
-/// </summary>
-public int[] TenRun(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// We'll say that an element in an array is "alone" if there are values before and after it, and
+    /// those values are different from it. Return a version of the given array where every instance
+    /// of the given value which is alone is replaced by whichever value to its left or right is larger.
+    /// 
+    /// notAlone([1, 2, 3], 2) → [1, 3, 3]
+    /// notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
+    /// notAlone([3, 4], 3) → [3, 4]
+    /// </summary>
+    public int[] NotAlone(int[] nums, int val)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Given a non-empty array of ints, return a new array containing the elements from the
-/// original array that come before the first 4 in the original array. The original array will
-/// contain at least one 4. Note that it is possible to create an array of length 0.
-/// 
-/// pre4([1, 2, 4, 1]) → [1, 2]
-/// pre4([3, 1, 4]) → [3, 1]
-/// pre4([1, 4, 4]) → [1]
-/// </summary>
-public int[] Pre4(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return an array that contains the exact same numbers as the given array, but rearranged
+    /// so that all the zeros are grouped at the start of the array. The order of the non-zero
+    /// numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and
+    /// return the given array or make a new array.
+    /// 
+    /// zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
+    /// zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
+    /// zeroFront([1, 0]) → [0, 1]
+    /// </summary>
+    public int[] ZeroFront(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Given a non-empty array of ints, return a new array containing the elements from the
-/// original array that come after the last 4 in the original array. The original array will contain
-/// at least one 4. Note that it is possible to create an array of length 0.
-/// 
-/// post4([2, 4, 1, 2]) → [1, 2]
-/// post4([4, 1, 4, 2]) → [2]
-/// post4([4, 4, 1, 2, 3]) → [1, 2, 3]
-/// </summary>
-public int[] Post4(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return a version of the given array where all the 10's have been removed. The remaining
+    /// elements should shift left towards the start of the array as needed, and the empty spaces
+    /// a the end of the array should be 0. So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify
+    /// and return the given array or make a new array.
+    /// 
+    /// withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
+    /// withoutTen([10, 2, 10]) → [2, 0, 0]
+    /// withoutTen([1, 99, 10]) → [1, 99, 0]
+    /// </summary>
+    public int[] WithoutTen(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// We'll say that an element in an array is "alone" if there are values before and after it, and
-/// those values are different from it. Return a version of the given array where every instance
-/// of the given value which is alone is replaced by whichever value to its left or right is larger.
-/// 
-/// notAlone([1, 2, 3], 2) → [1, 3, 3]
-/// notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
-/// notAlone([3, 4], 3) → [3, 4]
-/// </summary>
-public int[] NotAlone(int[] nums, int val)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return a version of the given array where each zero value in the array is replaced by the
+    /// largest odd value to the right of the zero in the array. If there is no odd value to the right
+    /// of the zero, leave the zero as a zero.
+    /// 
+    /// zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
+    /// zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
+    /// zeroMax([0, 1, 0]) → [1, 1, 0]
+    /// </summary>
+    public int[] ZeroMax(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return an array that contains the exact same numbers as the given array, but rearranged
-/// so that all the zeros are grouped at the start of the array. The order of the non-zero
-/// numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and
-/// return the given array or make a new array.
-/// 
-/// zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
-/// zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
-/// zeroFront([1, 0]) → [0, 1]
-/// </summary>
-public int[] ZeroFront(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return an array that contains the exact same numbers as the given array, but rearranged
+    /// so that all the even numbers come before all the odd numbers. Other than that, the
+    /// numbers can be in any order. You may modify and return the given array, or make a new
+    /// array.
+    /// 
+    /// evenOdd([1, 0, 1, 0, 0, 1, 1]) → [0, 0, 0, 1, 1, 1, 1]
+    /// evenOdd([3, 3, 2]) → [2, 3, 3]
+    /// evenOdd([2, 2, 2]) → [2, 2, 2]
+    /// </summary>
+    public int[] EvenOdd(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return a version of the given array where all the 10's have been removed. The remaining
-/// elements should shift left towards the start of the array as needed, and the empty spaces
-/// a the end of the array should be 0. So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify
-/// and return the given array or make a new array.
-/// 
-/// withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
-/// withoutTen([10, 2, 10]) → [2, 0, 0]
-/// withoutTen([1, 99, 10]) → [1, 99, 0]
-/// </summary>
-public int[] WithoutTen(int[] nums)
-{
-    throw new NotImplementedException();
-}
-
-/// <summary>
-/// Return a version of the given array where each zero value in the array is replaced by the
-/// largest odd value to the right of the zero in the array. If there is no odd value to the right
-/// of the zero, leave the zero as a zero.
-/// 
-/// zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
-/// zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
-/// zeroMax([0, 1, 0]) → [1, 1, 0]
-/// </summary>
-public int[] ZeroMax(int[] nums)
-{
-    throw new NotImplementedException();
-}
-
-/// <summary>
-/// Return an array that contains the exact same numbers as the given array, but rearranged
-/// so that all the even numbers come before all the odd numbers. Other than that, the
-/// numbers can be in any order. You may modify and return the given array, or make a new
-/// array.
-/// 
-/// evenOdd([1, 0, 1, 0, 0, 1, 1]) → [0, 0, 0, 1, 1, 1, 1]
-/// evenOdd([3, 3, 2]) → [2, 3, 3]
-/// evenOdd([2, 2, 2]) → [2, 2, 2]
-/// </summary>
-public int[] EvenOdd(int[] nums)
-{
-    throw new NotImplementedException();
-}
-
-/// <summary>
-/// This is slightly more difficult version of the famous FizzBuzz problem which is sometimes
-/// given as a first problem for job interviews. Consider the series
-/// of numbers beginning at start and running up to but not including end, so for example
-/// start=1 and end=5 gives the series 1, 2, 3, 4. Return a new String[] array containing the
-/// string form of these numbers, except for multiples of 3, use "Fizz" instead of the number,
-/// for multiples of 5 use "Buzz", and for multiples of both 3 and 5 use "FizzBuzz".
-/// This version is a little more complicated than the usual version since you have to
-/// allocate and index into an arrayinstead of just printing, and we vary the start/end instead of just always doing 1..100.
-/// 
-/// fizzBuzz(1, 6) → ["1", "2", "Fizz", "4", "Buzz"]
-/// fizzBuzz(1, 8) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7"]
-/// fizzBuzz(1, 11) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
-/// </summary>
-public string[] FizzBuzz(int start, int end)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// This is slightly more difficult version of the famous FizzBuzz problem which is sometimes
+    /// given as a first problem for job interviews. Consider the series
+    /// of numbers beginning at start and running up to but not including end, so for example
+    /// start=1 and end=5 gives the series 1, 2, 3, 4. Return a new String[] array containing the
+    /// string form of these numbers, except for multiples of 3, use "Fizz" instead of the number,
+    /// for multiples of 5 use "Buzz", and for multiples of both 3 and 5 use "FizzBuzz".
+    /// This version is a little more complicated than the usual version since you have to
+    /// allocate and index into an arrayinstead of just printing, and we vary the start/end instead of just always doing 1..100.
+    /// 
+    /// fizzBuzz(1, 6) → ["1", "2", "Fizz", "4", "Buzz"]
+    /// fizzBuzz(1, 8) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7"]
+    /// fizzBuzz(1, 11) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
+    /// </summary>
+    public string[] FizzBuzz(int start, int end)
+    {
+        throw new NotImplementedException();
+    }
 }
 
