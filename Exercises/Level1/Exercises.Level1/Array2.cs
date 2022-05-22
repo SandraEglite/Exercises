28fbca49577be95498a045901a934acb677389dd
Sandra Eglīte <san.eglite@gmail.com>
2022-05-22 12:51:41 +0300

    22May edit1

--- Exercises/Level1/Exercises.Level1/Array2.cs
+++ Exercises/Level1/Exercises.Level1/Array2.cs
@@ -571,182 +571,207 @@ public class Array2
                 return true;
             }
         }
-    
+
         return false;
     }
 
-/// <summary>
-/// Given start and end numbers, return a new array containing the sequence of integers
-/// rom start up to but not including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}. The
-/// end number will be greater or equal to the start number. Note that a length-0 array is
-/// valid.
-/// 
-/// fizzArray3(5, 10) → [5, 6, 7, 8, 9]
-/// fizzArray3(11, 18) → [11, 12, 13, 14, 15, 16, 17]
-/// fizzArray3(1, 3) → [1, 2]
-/// </summary>
-public int[] FizzArray3(int start, int end)
-{
-    int lenght = end - start;
-    int[] created = new int[lenght];
-
-    for (int i = 0; i < lenght; i++)
+    /// <summary>
+    /// Given start and end numbers, return a new array containing the sequence of integers
+    /// rom start up to but not including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}. The
+    /// end number will be greater or equal to the start number. Note that a length-0 array is
+    /// valid.
+    /// 
+    /// fizzArray3(5, 10) → [5, 6, 7, 8, 9]
+    /// fizzArray3(11, 18) → [11, 12, 13, 14, 15, 16, 17]
+    /// fizzArray3(1, 3) → [1, 2]
+    /// </summary>
+    public int[] FizzArray3(int start, int end)
     {
-        created[i] = start;
+        int lenght = end - start;
+        int[] created = new int[lenght];
+
+        for (int i = 0; i < lenght; i++)
         {
-            start++;
+            created[i] = start; //kāpēc šis
+            {
+                start++;
+            }
         }
+
+        return created;
+
     }
 
-    return created;
+    /// <summary>
+    /// Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You
+    /// may modify and return the given array, or return a new array.
+    /// 
+    /// shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
+    /// shiftLeft([1, 2]) → [2, 1]
+    /// shiftLeft([1]) → [1]
+    /// </summary>
+    public int[] ShiftLeft(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-}
+    /// <summary>
+    /// For each multiple of 10 in the given array, change all the values following it to be that
+    /// multiple of 10, until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2,
+    /// 10, 10, 10, 20, 20}.
+    /// 
+    /// tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+    /// tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
+    /// tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
+    /// </summary>
+    public int[] TenRun(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You
-/// may modify and return the given array, or return a new array.
-/// 
-/// shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
-/// shiftLeft([1, 2]) → [2, 1]
-/// shiftLeft([1]) → [1]
-/// </summary>
-public int[] ShiftLeft(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Given a non-empty array of ints, return a new array containing the elements from the
+    /// original array that come before the first 4 in the original array. The original array will
+    /// contain at least one 4. Note that it is possible to create an array of length 0.
+    /// 
+    /// pre4([1, 2, 4, 1]) → [1, 2]
+    /// pre4([3, 1, 4]) → [3, 1]
+    /// pre4([1, 4, 4]) → [1]
+    /// </summary>
+    public int[] Pre4(int[] nums)
+    {
+        int firstFour = 0;
+        int[] created;
 
-/// <summary>
-/// For each multiple of 10 in the given array, change all the values following it to be that
-/// multiple of 10, until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2,
-/// 10, 10, 10, 20, 20}.
-/// 
-/// tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
-/// tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
-/// tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
-/// </summary>
-public int[] TenRun(int[] nums)
-{
-    throw new NotImplementedException();
-}
+        for (int i = 0; nums[i] != 4; i++)
+        {
+            firstFour++;
+        }
 
-/// <summary>
-/// Given a non-empty array of ints, return a new array containing the elements from the
-/// original array that come before the first 4 in the original array. The original array will
-/// contain at least one 4. Note that it is possible to create an array of length 0.
-/// 
-/// pre4([1, 2, 4, 1]) → [1, 2]
-/// pre4([3, 1, 4]) → [3, 1]
-/// pre4([1, 4, 4]) → [1]
-/// </summary>
-public int[] Pre4(int[] nums)
-{
-    throw new NotImplementedException();
-}
+        created = new int[firstFour];
 
-/// <summary>
-/// Given a non-empty array of ints, return a new array containing the elements from the
-/// original array that come after the last 4 in the original array. The original array will contain
-/// at least one 4. Note that it is possible to create an array of length 0.
-/// 
-/// post4([2, 4, 1, 2]) → [1, 2]
-/// post4([4, 1, 4, 2]) → [2]
-/// post4([4, 4, 1, 2, 3]) → [1, 2, 3]
-/// </summary>
-public int[] Post4(int[] nums)
-{
-    throw new NotImplementedException();
-}
+        for (int i = 0; i < firstFour; i++)
+        {
+            created[i] = nums[i];//kāpēc šis?
+        }
+        return created;
+    }
 
-/// <summary>
-/// We'll say that an element in an array is "alone" if there are values before and after it, and
-/// those values are different from it. Return a version of the given array where every instance
-/// of the given value which is alone is replaced by whichever value to its left or right is larger.
-/// 
-/// notAlone([1, 2, 3], 2) → [1, 3, 3]
-/// notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
-/// notAlone([3, 4], 3) → [3, 4]
-/// </summary>
-public int[] NotAlone(int[] nums, int val)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Given a non-empty array of ints, return a new array containing the elements from the
+    /// original array that come after the last 4 in the original array. The original array will contain
+    /// at least one 4. Note that it is possible to create an array of length 0.
+    /// 
+    /// post4([2, 4, 1, 2]) → [1, 2]
+    /// post4([4, 1, 4, 2]) → [2]
+    /// post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+    /// </summary>
+    public int[] Post4(int[] nums)
+    {
+        throw new NotImplementedException();
 
-/// <summary>
-/// Return an array that contains the exact same numbers as the given array, but rearranged
-/// so that all the zeros are grouped at the start of the array. The order of the non-zero
-/// numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and
-/// return the given array or make a new array.
-/// 
-/// zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
-/// zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
-/// zeroFront([1, 0]) → [0, 1]
-/// </summary>
-public int[] ZeroFront(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    }
 
-/// <summary>
-/// Return a version of the given array where all the 10's have been removed. The remaining
-/// elements should shift left towards the start of the array as needed, and the empty spaces
-/// a the end of the array should be 0. So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify
-/// and return the given array or make a new array.
-/// 
-/// withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
-/// withoutTen([10, 2, 10]) → [2, 0, 0]
-/// withoutTen([1, 99, 10]) → [1, 99, 0]
-/// </summary>
-public int[] WithoutTen(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// We'll say that an element in an array is "alone" if there are values before and after it, and
+    /// those values are different from it. Return a version of the given array where every instance
+    /// of the given value which is alone is replaced by whichever value to its left or right is larger.
+    /// 
+    /// notAlone([1, 2, 3], 2) → [1, 3, 3]
+    /// notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
+    /// notAlone([3, 4], 3) → [3, 4]
+    /// </summary>
+    public int[] NotAlone(int[] nums, int val)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return a version of the given array where each zero value in the array is replaced by the
-/// largest odd value to the right of the zero in the array. If there is no odd value to the right
-/// of the zero, leave the zero as a zero.
-/// 
-/// zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
-/// zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
-/// zeroMax([0, 1, 0]) → [1, 1, 0]
-/// </summary>
-public int[] ZeroMax(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return an array that contains the exact same numbers as the given array, but rearranged
+    /// so that all the zeros are grouped at the start of the array. The order of the non-zero
+    /// numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and
+    /// return the given array or make a new array.
+    /// 
+    /// zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
+    /// zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
+    /// zeroFront([1, 0]) → [0, 1]
+    /// </summary>
+    public int[] ZeroFront(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
 
-/// <summary>
-/// Return an array that contains the exact same numbers as the given array, but rearranged
-/// so that all the even numbers come before all the odd numbers. Other than that, the
-/// numbers can be in any order. You may modify and return the given array, or make a new
-/// array.
-/// 
-/// evenOdd([1, 0, 1, 0, 0, 1, 1]) → [0, 0, 0, 1, 1, 1, 1]
-/// evenOdd([3, 3, 2]) → [2, 3, 3]
-/// evenOdd([2, 2, 2]) → [2, 2, 2]
-/// </summary>
-public int[] EvenOdd(int[] nums)
-{
-    throw new NotImplementedException();
-}
+    /// <summary>
+    /// Return a version of the given array where all the 10's have been removed. The remaining
+    /// elements should shift left towards the start of the array as needed, and the empty spaces
+    /// a the end of the array should be 0. So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify
+    /// and return the given array or make a new array.
+    /// 
+    /// withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
+    /// withoutTen([10, 2, 10]) → [2, 0, 0]
+    /// withoutTen([1, 99, 10]) → [1, 99, 0]
+    /// </summary>
+    public int[] WithoutTen(int[] nums)
+    {
+        int tens = 0;
+        int[] created = new int[nums.Length];
 
-/// <summary>
-/// This is slightly more difficult version of the famous FizzBuzz problem which is sometimes
-/// given as a first problem for job interviews. Consider the series
-/// of numbers beginning at start and running up to but not including end, so for example
-/// start=1 and end=5 gives the series 1, 2, 3, 4. Return a new String[] array containing the
-/// string form of these numbers, except for multiples of 3, use "Fizz" instead of the number,
-/// for multiples of 5 use "Buzz", and for multiples of both 3 and 5 use "FizzBuzz".
-/// This version is a little more complicated than the usual version since you have to
-/// allocate and index into an arrayinstead of just printing, and we vary the start/end instead of just always doing 1..100.
-/// 
-/// fizzBuzz(1, 6) → ["1", "2", "Fizz", "4", "Buzz"]
-/// fizzBuzz(1, 8) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7"]
-/// fizzBuzz(1, 11) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
-/// </summary>
-public string[] FizzBuzz(int start, int end)
-{
-    throw new NotImplementedException();
-}
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 10)
+            {
+                created[tens++] = nums[i];// kāpēc?
+            }
+        }
+        return created;
+    }
+
+    /// <summary>
+    /// Return a version of the given array where each zero value in the array is replaced by the
+    /// largest odd value to the right of the zero in the array. If there is no odd value to the right
+    /// of the zero, leave the zero as a zero.
+    /// 
+    /// zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
+    /// zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
+    /// zeroMax([0, 1, 0]) → [1, 1, 0]
+    /// </summary>
+    public int[] ZeroMax(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Return an array that contains the exact same numbers as the given array, but rearranged
+    /// so that all the even numbers come before all the odd numbers. Other than that, the
+    /// numbers can be in any order. You may modify and return the given array, or make a new
+    /// array.
+    /// 
+    /// evenOdd([1, 0, 1, 0, 0, 1, 1]) → [0, 0, 0, 1, 1, 1, 1]
+    /// evenOdd([3, 3, 2]) → [2, 3, 3]
+    /// evenOdd([2, 2, 2]) → [2, 2, 2]
+    /// </summary>
+    public int[] EvenOdd(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// This is slightly more difficult version of the famous FizzBuzz problem which is sometimes
+    /// given as a first problem for job interviews. Consider the series
+    /// of numbers beginning at start and running up to but not including end, so for example
+    /// start=1 and end=5 gives the series 1, 2, 3, 4. Return a new String[] array containing the
+    /// string form of these numbers, except for multiples of 3, use "Fizz" instead of the number,
+    /// for multiples of 5 use "Buzz", and for multiples of both 3 and 5 use "FizzBuzz".
+    /// This version is a little more complicated than the usual version since you have to
+    /// allocate and index into an arrayinstead of just printing, and we vary the start/end instead of just always doing 1..100.
+    /// 
+    /// fizzBuzz(1, 6) → ["1", "2", "Fizz", "4", "Buzz"]
+    /// fizzBuzz(1, 8) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7"]
+    /// fizzBuzz(1, 11) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
+    /// </summary>
+    public string[] FizzBuzz(int start, int end)
+    {
+        throw new NotImplementedException();
+    }
 }
 
